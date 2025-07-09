c4b005c49935f7937caf8b598baaab50238ffb75
Yemmala Lekhasree <lekhasree0910@gmail.com>
2025-07-09 20:12:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Get a reference to the button element by its ID
+const button = document.getElementById('alertButton');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    // This function will be executed when the button is clicked
+    alert('Hello! You just clicked the button!');
+});
+
+// You can add more interactive elements here
+console.log('JavaScript is running!');
